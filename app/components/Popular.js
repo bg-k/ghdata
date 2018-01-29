e429b3dd3bd3db47eb57d254e3904029ed98d2dc
ryo makabe <penginryo@gmail.com>
2018-01-28 23:26:38 -0800

    build poular navigation

---
+++ app/components/Popular.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const SelectedLanguage =  (props) => {
+  let languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+  return(
+    <ul className='languages'>
+      {languages.map((lang) => {
+        return (
+          <li
+            style={lang === props.selectedLanguage ? { color: '#d0021b'} : null}
+            onClick={props.onSelect.bind(null, lang)}
+            key={lang}>
+            {lang}
+          </li>
+        );
+      })}
+    </ul>
+  )
+}
+
+SelectedLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired,
+};
+
+class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: 'All'
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+  updateLanguage(lang) {
+    this.setState(() => {
+      return {
+        selectedLanguage: lang
+      }
+    });
+  }
+  render() {
+    return(
+      <div>
+        <SelectedLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage}
+        />
+      </div>
+    );
+  }
+}
+
+export default Popular;
--- app/index.js
+++ app/index.js
@@ -1,16 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-class App extends React.Component {
-  render() {
-    return(
-      <div>
-        FUCK OFF
-      </div>
-    );
-  }
-}
+import App from './components/App';
 
 ReactDOM.render(
   <App />,
